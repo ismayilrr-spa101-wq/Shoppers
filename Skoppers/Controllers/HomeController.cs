29593022c38682a97c573d55809b0bab752ec65b
ismayilrr-spa101-wq <ismayilrr-spa101@code.edu.az>
2026-01-01 02:33:15 +0400

    first commit

---
+++ Skoppers/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skoppers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
